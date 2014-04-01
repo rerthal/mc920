4d04350b0b4b24015f1bfc680f4c1b12859b1c0c
Martin Ichilevici de Oliveira <martin.i.oliveira@gmail.com>
2014-04-01 01:27:59 -0300

    proj08: Added mssim

--- proj08/proj08.py
+++ proj08/proj08.py
@@ -3,6 +3,14 @@ import cv2
 import sys
 import math
 
+L=256
+k1 = 0.01
+k2 = 0.03
+k3 = k2/2
+c1 = k1*L
+c2 = k2*L
+c3 = k3*L
+
 def total_error(x, y):
     return sum(sum((x-y)**2))
 
@@ -13,9 +21,21 @@ def snr_error(x, y):
     denominator = sum(sum(y**2))
     return float(denominator) / total_error(x, y)
 
-def ssim_error(src, filtered, alpha=1, beta=1, gamma=1, c1=0, c2=0, c3=0):
-    return pow(luminance(src, filtered, c1), alpha) * pow(contrast(src, filtered, c2), beta) * pow(structure(src, filtered, c3), gamma)
-    return 0
+def ssim(x, y, alpha=1, beta=1, gamma=1, c1=c1, c2=c2, c3=c3):
+    return pow(luminance(x, y, c1), alpha) * pow(contrast(x, y, c2), beta) * pow(structure(x, y, c3), gamma)
+
+def mssim(x, y, window_size=11, alpha=1, beta=1, gamma=1, c1=c1, c2=c2, c3=c3):
+    n = window_size
+    M = 0.
+    _mssim = 0.
+    for i in range(n/2, x.shape[0]-n/2):
+        for j in range(n/2, x.shape[1]-n/2):
+            subx = x[i-n/2:i+n/2+1, j-n/2:j+n/2+1]
+            suby = y[i-n/2:i+n/2+1, j-n/2:j+n/2+1]
+            _mssim += ssim(subx, suby, alpha, beta, gamma, c1, c2, c3)
+            M += 1
+    return _mssim/M
+
 
 def luminance(x, y, c=0):
     mu_x = x.mean()
@@ -101,6 +121,7 @@ ga_median = cv2.medianBlur(ga, 3)
 ga_gaussian = cv2.GaussianBlur(ga, ksize=(3,3), sigmaX=135)
 ga_aniso = aniso_diff(ga)
 
+'''
 cv2.imwrite('src.jpg',src)
 cv2.imwrite('sp.jpg',sp)
 cv2.imwrite('sp_median.jpg',sp_median)
@@ -110,33 +131,47 @@ cv2.imwrite('ga.jpg',ga)
 cv2.imwrite('ga_median.jpg',ga_median)
 cv2.imwrite('ga_gaussian.jpg',ga_gaussian)
 cv2.imwrite('ga_aniso.jpg',ga_aniso)
+'''
 
-print "Filtro gaussiano sobre ruido gaussiano         & %d & %.3f & %.3f & %.3f \\\\\hline" % (total_error(src, ga_gaussian), rms_error(src, ga_gaussian), snr_error(src, ga_gaussian), ssim_error(src, ga_gaussian))
+#print mssim(src, ga_gaussian, 11)
+#print mssim(src, sp)
+#print mssim(src, sp_median)
+#print mssim(src, sp_gaussian)
+#print mssim(src, sp_aniso)
+#print mssim(src, ga)
+#print mssim(src, ga_median)
+#print mssim(src, ga_gaussian)
+#print mssim(src, ga_aniso)
 
-print "Filtro gaussiano sobre ruido sal e pimenta     & %d & %.3f & %.3f & %.3f \\\\\hline" % (total_error(src, sp_gaussian), rms_error(src, sp_gaussian), snr_error(src, sp_gaussian), ssim_error(src, sp_gaussian))
 
-print "Filtro da mediana sobre ruido gaussiano        & %d & %.3f & %.3f & %.3f \\\\\hline" % (total_error(src, ga_median), rms_error(src, ga_median), snr_error(src, ga_median), ssim_error(src, ga_median))
+print "Filtro gaussiano sobre ruido gaussiano         & %d & %.3f & %.3f & %.3f \\\\\hline" % (total_error(src, ga_gaussian), rms_error(src, ga_gaussian), snr_error(src, ga_gaussian), ssim(src, ga_gaussian, c1=c1, c2=c2, c3=c3))
 
-print "Filtro da mediana sobre ruido sal e pimenta    & %d & %.3f & %.3f & %.3f \\\\\hline" % (total_error(src, sp_median), rms_error(src, sp_median), snr_error(src, sp_median), ssim_error(src, sp_median))
+print "Filtro gaussiano sobre ruido sal e pimenta     & %d & %.3f & %.3f & %.3f \\\\\hline" % (total_error(src, sp_gaussian), rms_error(src, sp_gaussian), snr_error(src, sp_gaussian), ssim(src, sp_gaussian, c1=c1, c2=c2, c3=c3))
 
-print "Difusao anisotropica sobre ruido gaussiano     & %d & %.3f & %.3f & %.3f \\\\\hline" % (total_error(src, ga_aniso), rms_error(src, ga_aniso), snr_error(src, ga_aniso), ssim_error(src, ga_aniso))
+print "Filtro da mediana sobre ruido gaussiano        & %d & %.3f & %.3f & %.3f \\\\\hline" % (total_error(src, ga_median), rms_error(src, ga_median), snr_error(src, ga_median), ssim(src, ga_median, c1=c1, c2=c2, c3=c3))
 
-print "Difusao anisotropica sobre ruido sal e pimenta & %d & %.3f & %.3f & %.3f \\\\\hline" % (total_error(src, sp_aniso), rms_error(src, sp_aniso), snr_error(src, sp_aniso), ssim_error(src, sp_aniso))
+print "Filtro da mediana sobre ruido sal e pimenta    & %d & %.3f & %.3f & %.3f \\\\\hline" % (total_error(src, sp_median), rms_error(src, sp_median), snr_error(src, sp_median), ssim(src, sp_median, c1=c1, c2=c2, c3=c3))
 
+print "Difusao anisotropica sobre ruido gaussiano     & %d & %.3f & %.3f & %.3f \\\\\hline" % (total_error(src, ga_aniso), rms_error(src, ga_aniso), snr_error(src, ga_aniso), ssim(src, ga_aniso, c1=c1, c2=c2, c3=c3))
+
+print "Difusao anisotropica sobre ruido sal e pimenta & %d & %.3f & %.3f & %.3f \\\\\hline" % (total_error(src, sp_aniso), rms_error(src, sp_aniso), snr_error(src, sp_aniso), ssim(src, sp_aniso, c1=c1, c2=c2, c3=c3))
+
+'''
 print "niter graph"
 for x in range(1,500,50):
     filtered = aniso_diff(ga, niter=x)
-    error    = ssim_error(src, filtered)
+    error    = ssim(src, filtered)
     print "(", x, ",", error, ")"
 
 print "kappa graph"
 for x in range(1,500,50):
     filtered = aniso_diff(ga, kappa=x)
-    error    = ssim_error(src, filtered)
+    error    = ssim(src, filtered)
     print "(", x, ",", error, ")"
 
 print "gamma graph"
 for x in [z/500.0 for z in range(1, 500,50)]:
     filtered = aniso_diff(ga, gamma=x)
-    error    = ssim_error(src, filtered)
+    error    = ssim(src, filtered)
     print "(", x, ",", error, ")"
+'''
