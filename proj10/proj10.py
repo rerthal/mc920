f8433d2ef628ba893d5f5b9f0a00cbec64e4979d
lamasu <rafaerthal@gmail.com>
2014-05-06 14:29:31 -0300

    fixing fft unshift

--- proj10/proj10.py
+++ proj10/proj10.py
@@ -3,24 +3,29 @@ import numpy as np
 import cv2
 import math
 
+import matplotlib.pyplot as plt
+
 def even_slices(image, M=8):
     for i in range(1, image.shape[0] / M):
         for j in range(1, image.shape[1] / M):
             if i % 2 == 0 and j % 2 == 0:
-                sub_image = [line[(i-1)*M: (i+1)*M] for line in image[(j-1)*M: (j+1)*M]]
+                sub_image = np.array(image)[(i-1)*M: (i+1)*M, (j-1)*M: (j+1)*M]
                 if len(sub_image) > 0: yield (np.asarray(sub_image), i, j)
 
 def max_frequency(image,M=8):
-    max = (0,0,0)
-    for i in range(1, len(image)):
-        for j in range(1, len(image[i])):
-            if abs(image[i][j]) > max[2]:
-                if(i,j) != (M,M): max = (i-M, j-M, abs(image[i][j]))
-    return int(np.linalg.norm([max[0] - M, max[1] - M]))
+    centre=(M,M)
+    max = (0,0,0,0)
+    for i in range(1,len(image)):
+        for j in range(1,len(image[i])):
+            norm = abs(image[i][j])**2
+            if norm > max[2] and (i,j) != centre:
+                dist = math.sqrt((i-centre[0])**2 + (j-centre[1])**2)
+                max = (i-centre[0],j-centre[1],norm,dist)
+    return int(math.ceil(max[3]))
 
 def Npb(image):
     circle = np.ones(image.shape)
-    cv2.circle(circle, image.shape, max_frequency(image), color=0)
+    cv2.circle(circle, (image.shape[0]/2, image.shape[1]/2), max_frequency(image), color=0)
     removed_ring = image*circle
     removed_ring[(image.shape[0] / 2, image.shape[1] / 2)] = 0
     percentile = np.percentile(removed_ring, 5)
@@ -33,34 +38,34 @@ def HMb(region, M=8):
     for i in range(len(region)):
         for j in range(len(region[i])):
             if abs(region[i][j]) >= npb: hmb[i][j] = region[i][j]
-    return np.fft.ifft2(hmb)
+    return hmb
 
 def Hghb(region, M=8):
     result = np.ones((2*M,2*M), dtype=complex)
     for i in range(len(region)):
         for j in range(len(region[i])):
-            d = np.linalg.norm([i - image.shape[0]/2, j - image.shape[1]/2])
-            Hlp = math.exp((-1/2.0)*(d)/((max_frequency(image)+image.shape[0]/16)**2))
-            Hhp = 1 - math.exp((-1/2.0)*(d)/((max_frequency(image))**2))
+            d = np.linalg.norm([i - region.shape[0]/2, j - region.shape[1]/2])
+            Hlp = math.exp((-1/2.0)*(d**2)/((max_frequency(region)+region.shape[0]/16)**2))
+            Hhp = 1 - math.exp((-1/2.0)*(d**2)/((max_frequency(region))**2))
             result[i][j] = Hlp * Hhp
-    return np.fft.ifft2(result)
+    return result * HMb(region)
 
 if __name__ == '__main__':
-    for fingerprint in glob.glob("Fingerprints/*.tif"):
+    #for fingerprint in glob.glob("Fingerprints/*.tif"):
+        fingerprint = "Fingerprints/1_1.tif"
         image = cv2.cvtColor(cv2.imread(fingerprint), cv2.cv.CV_BGR2GRAY)
-        intermediate_image = np.zeros((400,400))
-        final_image = np.zeros((400,400))
+        intermediate_image = np.zeros(image.shape)
+        final_image = np.zeros(image.shape)
         for region, ki, kj in even_slices(image):
             fft = np.fft.fft2(region)
             fft_shift = np.fft.fftshift(fft)
-            hmb = HMb(fft_shift)
-            hghb = Hghb(fft_shift)
+            hmb = np.fft.ifft2(np.fft.ifftshift(HMb(fft_shift)))
+            hghb = np.fft.ifft2(np.fft.ifftshift(Hghb(fft_shift)))
             hmb_intensity = (hmb * 255) / (np.amax(hmb))
-            hghb_intensity = (hghb * 255) / (np.amax(hghb))
-            for j in range(16):
-                for i in range(16):
-                    intermediate_image[(kj) * 8 + j][(ki) * 8 + i] = abs(hmb_intensity[j][i])
-                    final_image[(kj) * 8 + j][(ki) * 8 + i] = abs(hghb_intensity[j][i])
+            for i in range(16):
+                for j in range(16):
+                    intermediate_image[(ki) * 8 + i][(kj) * 8 + j] = abs(hmb_intensity[i][j])
+                    final_image[(ki) * 8 + i][(kj) * 8 + j] = hghb[i][j]
         cv2.imwrite(fingerprint[:-4] + '_intermediate.jpg', intermediate_image)
         cv2.imwrite(fingerprint[:-4] + '_final.jpg', final_image)
         print fingerprint[:-4]
